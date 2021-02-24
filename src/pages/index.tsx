da0f7dab8ec3b99353b5d3947e652d89aade2c69
Gabriel Barboza <gbds1703farra@hotmail.com>
2021-02-24 00:08:35 -0300

    Projeto em desenvolvimento no NLW#4 da RocketSeat.

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { CompletedChallenges } from "../components/ComplitedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import styles from '../styles/pages/Home.module.css';
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Moveit</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  );
+}
