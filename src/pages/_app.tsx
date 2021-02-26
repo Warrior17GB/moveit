40ae084a261b8717f94bec5908fe5780f8e3222a
Gabriel Barboza <gbds1703farra@hotmail.com>
2021-02-25 22:41:41 -0300

    Finalizadas funções do Countdown e implementadas funções de level e desafios.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/global.css';
 import { ChallengesProvider } from '../contexts/ChallengesContext'
+import React from 'react';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 function MyApp({ Component, pageProps }) {
   return (
